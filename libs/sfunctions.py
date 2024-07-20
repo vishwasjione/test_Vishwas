18971777830cc0a26088ae86321e12ef70cb820b
Vishwas <vishwasjione@gmail.com>
2024-07-20 12:30:17 -0400

    chore: Add basic functionality to calculate and print the sum of two numbers

---
+++ libs/sfunctions.py
@@ -0,0 +1,7 @@
+def add_two_numbers(number1, number2):
+
+    return number1 + number2
+
+def multiply_two_numbers(number1, number2):
+
+    return number1 * number2
---
+++ use_add_function.py
@@ -0,0 +1,9 @@
+from libs.sfunctions import add_two_numbers
+from libs.sfunctions import multiply_two_numbers
+
+# Example usage
+result = add_two_numbers(5, 7)
+print(f"The result is: {result}")
+
+result = multiply_two_numbers(5, 7)
+print(f"The result is: {result}")
